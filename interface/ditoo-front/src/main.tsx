1f26177bb3d0ca95fe567b3b659e7e477f0cea90
ByakkoLord <g.fonsecadacosta@gmail.com>
2026-03-30 00:23:24 -0300

    refatoração: substituído componente App por rotas de navegação para Chat e LoginPage

--- interface/ditoo-front/src/main.tsx
+++ interface/ditoo-front/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from "react-router";
+import './assets/index.css'
+import './assets/loginPage.css'
+import './assets/fonts.css'
+import Chat from "./pages/Chat"
+import LoginPage from './pages/LoginPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/chat" element={<Chat />} />  
+        <Route path="/login" element={<LoginPage />} />  
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
