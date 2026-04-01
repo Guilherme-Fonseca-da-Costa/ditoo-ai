0491cb7b2d3cfa925ac3bb77e08b243b89b1c7fe
ByakkoLord <g.fonsecadacosta@gmail.com>
2026-04-01 00:20:55 -0300

    Adicionado componente Config com estrutura básica para a página de configurações

---
+++ interface/ditoo-front/src/pages/Config.tsx
@@ -0,0 +1,8 @@
+export default function Config() {
+    return (
+        <div>
+            <div id="configSideBar"></div>
+            <div className="configBlock"></div>
+        </div>
+    );
+}
