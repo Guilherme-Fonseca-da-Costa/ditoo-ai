9e6f5d04deded46752743ad77824d49e0b2b1f5b
ByakkoLord <g.fonsecadacosta@gmail.com>
2026-04-02 01:46:55 -0300

    Adicionado rotas para as páginas de Configuração e Not Found

--- interface/ditoo-front/src/main.tsx
+++ interface/ditoo-front/src/main.tsx
@@ -4,8 +4,11 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import './assets/index.css'
 import './assets/loginPage.css'
 import './assets/fonts.css'
+import './assets/configPage.css'
 import Chat from "./pages/Chat"
 import LoginPage from './pages/LoginPage.tsx';
+import Config from './pages/Config.tsx';
+import NotFound from './pages/NotFound.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -13,6 +16,8 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/chat" element={<Chat />} />  
         <Route path="/login" element={<LoginPage />} />  
+        <Route path="/config" element={<Config />} />
+         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
