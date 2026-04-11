107962c66a16ecd28b2c7b8921e6d8c278ff3585
ByakkoLord <g.fonsecadacosta@gmail.com>
2026-04-10 23:33:19 -0300

    Add proxy configuration for /changeModel endpoint

--- interface/ditoo-front/vite.config.ts
+++ interface/ditoo-front/vite.config.ts
@@ -15,6 +15,11 @@ export default defineConfig({
         target: 'http://127.0.0.1:8000',
         changeOrigin: true,
         secure: false,
+      },
+       '/changeModel': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        secure: false,
       }
     },
   },
